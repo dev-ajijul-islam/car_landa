4519a0a7be0297a64f6747765e5eb9df31752c7a
dev-ajijul-islam <devajijulislam@gmail.com>
2025-12-10 12:41:31 +0600

    choiceChip of history screen is created

--- lib/ui/screens/profile/my_history.dart
+++ lib/ui/screens/profile/my_history.dart
@@ -1,20 +1,64 @@
 import 'package:flutter/material.dart';
 
-class MyHistory extends StatelessWidget {
+class MyHistory extends StatefulWidget {
   const MyHistory({super.key});
 
   static String name = "my-history";
 
   @override
+  State<MyHistory> createState() => _MyHistoryState();
+}
+
+class _MyHistoryState extends State<MyHistory> {
+  final List<String> historySections = ["In Progress", "Delivered", "Canceled"];
+
+  int selectedChip = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My History"),
-      ),
-      body: Column(
-        children: [
-
-        ],
+      appBar: AppBar(title: Text("My History")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return ChoiceChip(
+                    showCheckmark: false,
+                    color: WidgetStateColor.resolveWith(
+                      (states) => selectedChip == index
+                          ? ColorScheme.of(context).primary
+                          : Colors.white,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    onSelected: (value) {
+                      setState(() {
+                        selectedChip = index;
+                      });
+                    },
+                    label: Text(
+                      historySections[index],
+                      style: TextStyle(
+                        color: selectedChip == index
+                            ? Colors.white
+                            : ColorScheme.of(context).primary,
+                      ),
+                    ),
+                    selected: selectedChip == index,
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(width: 10),
+                itemCount: historySections.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
