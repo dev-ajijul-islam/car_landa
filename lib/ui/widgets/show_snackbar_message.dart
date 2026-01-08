aff9a468b1ff6f94b9b43fb5c72e04a255257298
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-08 23:21:06 +0600

    snackbar widget translated

--- lib/ui/widgets/show_snackbar_message.dart
+++ lib/ui/widgets/show_snackbar_message.dart
@@ -1,15 +1,16 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 showSnackbarMessage({
-  required  context,
+  required BuildContext context,
   required String message,
   Color? color,
 }) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Text(
-        message,
-        style: TextStyle(fontSize: 14),
+        message.tr(),
+        style: const TextStyle(fontSize: 14),
         textAlign: TextAlign.center,
       ),
       backgroundColor: color,
