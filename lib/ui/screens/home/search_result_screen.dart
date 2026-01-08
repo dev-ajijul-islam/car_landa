e91464fa0cf5a7f1fc8a3293daf8ab278e56e6ea
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-08 22:22:42 +0600

    search result screen translated

--- lib/ui/screens/home/search_result_screen.dart
+++ lib/ui/screens/home/search_result_screen.dart
@@ -1,5 +1,6 @@
 import 'package:car_hub/providers/advance_search_provider.dart';
 import 'package:car_hub/ui/widgets/car_card.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,17 +12,17 @@ class SearchResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search Result", style: TextTheme.of(context).titleMedium),
+        title: Text("search_result.title".tr(), style: TextTheme.of(context).titleMedium),
       ),
       body: RefreshIndicator(
         onRefresh: () async {},
         child: Consumer<AdvanceSearchProvider>(
           builder: (context, provider, child) => ListView.separated(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             itemCount: provider.searchedCars.length,
             itemBuilder: (context, index) =>
                 CarCard(car: provider.searchedCars[index]),
-            separatorBuilder: (context, index) => SizedBox(height: 10),
+            separatorBuilder: (context, index) => const SizedBox(height: 10),
           ),
         ),
       ),
